89e8e9a9f8909837b6f07ec28ac03facc0134ea4
Joseph Sayekh <jsayek2@uic.edu>
2017-10-26 16:03:15 -0500

    Added functionality to identify which animals were "taken pictures" of

---
+++ PhotoSimVR/Assets/Camera_Listener.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Camera_Listener : MonoBehaviour {
+
+    public static bool pressed;
+
+	// Use this for initialization
+	void Start () {
+        if(GetComponent<VRTK_ControllerEvents>() == null)
+            {
+            VRTK_Logger.Error(VRTK_Logger.GetCommonMessage(VRTK_Logger.CommonMessageKeys.REQUIRED_COMPONENT_MISSING_FROM_GAMEOBJECT, "VRTK_ControllerEvents_ListenerExample", "VRTK_ControllerEvents", "the same"));
+            return;
+        }
+
+        GetComponent<VRTK_ControllerEvents>().GripPressed += Camera_Listener_GripPressed;
+
+    }
+
+    private void Camera_Listener_GripPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        if (TakePicture.triggered == true)
+        {
+            pressed = true;
+           //Debug.Log("pressed");
+        }
+    }
+}
---
+++ PhotoSimVR/Assets/TakePicture.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRTK;
+
+public class TakePicture : MonoBehaviour {
+
+    GameObject duck;
+    GameObject pig;
+    GameObject cow;
+    GameObject chicken;
+    GameObject sheep;
+    public static bool triggered;
+    public Text display;
+
+    void Start()
+    {
+        // Setting up the references.
+        duck = GameObject.FindGameObjectWithTag("duck");
+        pig = GameObject.FindGameObjectWithTag("pig");
+        cow = GameObject.FindGameObjectWithTag("cow");
+        chicken = GameObject.FindGameObjectWithTag("chicken");
+        sheep = GameObject.FindGameObjectWithTag("sheep");
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+       if(Camera_Listener.pressed == true)
+        { 
+            if (other.gameObject == duck)
+            {
+                display.text = "Duck";
+            }
+            else if (other.gameObject == pig)
+            {
+                display.text = "Pig";
+            }
+            else if (other.gameObject == cow)
+            {
+                display.text = "Cow";
+            }
+            else if (other.gameObject == chicken)
+            {
+                display.text = "Chicken";
+            }
+            else if (other.gameObject == sheep)
+            {
+                display.text = "Sheep";
+            }
+            Camera_Listener.pressed = false;
+       }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggered = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggered = false;
+    }
+}
