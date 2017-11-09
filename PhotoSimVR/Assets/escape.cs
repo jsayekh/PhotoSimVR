cb10a63bb048f5d8453c6bff66e81e3b1855aaee
Mac Squibb <jsquib2@uic.edu>
2017-11-09 14:04:53 -0600

    Added AI

---
+++ PhotoSimVR/Assets/escape.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class escape : MonoBehaviour {
+
+	public Transform player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Vector3.Distance (player.position, this.transform.position) < 10) {
+
+			//Vector3 direction = player.position - this.transform.position;
+			Vector3 direction = (this.transform.position - player.position) * -1;
+			direction.y = 0;
+
+			this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.LookRotation (direction), 0.1f);
+			this.transform.Translate (0, 0, 0.03f * -1);
+
+
+		}
+
+	}
+}
+
+
+
+
+	
